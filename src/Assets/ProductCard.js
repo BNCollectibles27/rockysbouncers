a8ec7097c19b781c0502157d9c538fe011610391
jamesnguyen03 <jamesnguyen99@hotmail.com>
2024-01-11 16:37:16 -0600

    fixed bug

--- src/Assets/ProductCard.js
+++ src/Assets/ProductCard.js
@@ -27,6 +27,7 @@ function ProductCard(props){
 
     return(
         <div class={props.class !== undefined ? `${props.class} product-card-container` : "product-card-container"}>
+            Hello
             <div class="product-image-container">
                 <img class="product-image" src={props.image} alt={props.alt} />
             </div>
